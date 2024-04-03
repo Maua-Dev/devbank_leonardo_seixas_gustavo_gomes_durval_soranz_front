af27e79ef1c1851f7d574bce29ffdd4148768517
lseixas <lseixas.iorio@gmail.com>
2024-04-03 19:56:35 -0300

    reup

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, ReactNode, useState } from "react";
 
 type VariaveisGlobaisType = {
   api: string;
@@ -9,19 +9,35 @@ type VariaveisGlobaisType = {
 
   setApi: React.Dispatch<React.SetStateAction<string>>;
   setNome: React.Dispatch<React.SetStateAction<string>>;
-  setAngencia: React.Dispatch<React.SetStateAction<string>>;
+  setAgencia: React.Dispatch<React.SetStateAction<string>>;
   setConta: React.Dispatch<React.SetStateAction<string>>;
   setCredito: React.Dispatch<React.SetStateAction<number>>;
 };
 
-export const VariaveisGlobais = createContext("");
+const defaultVariaveisGlobais: VariaveisGlobaisType = {
+  api: "",
+  nome: "",
+  agencia: "",
+  conta: "",
+  credito: 0,
 
-export const PegarVariaveis = ({ children }) => {
-  const [api, setApi] = useState();
-  const [nome, setNome] = useState();
-  const [agencia, setAgencia] = useState();
-  const [conta, setConta] = useState();
-  const [credito, setCredito] = useState();
+  setApi: () => {},
+  setNome: () => {},
+  setAgencia: () => {},
+  setConta: () => {},
+  setCredito: () => {},
+};
+
+export const VariaveisGlobais = createContext<VariaveisGlobaisType>(
+  defaultVariaveisGlobais
+);
+
+export const PegarVariaveis = ({ children }: { children: ReactNode }) => {
+  const [api, setApi] = useState<string>("");
+  const [nome, setNome] = useState<string>("");
+  const [agencia, setAgencia] = useState<string>("");
+  const [conta, setConta] = useState<string>("");
+  const [credito, setCredito] = useState<number>(0);
 
   return (
     <VariaveisGlobais.Provider
