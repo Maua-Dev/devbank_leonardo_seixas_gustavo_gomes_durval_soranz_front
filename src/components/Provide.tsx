6fad034fafcdc58267f7034dc018f1b3d8e8e826
lseixas <lseixas.iorio@gmail.com>
2024-04-04 18:00:20 -0300

    decoração primeira página

--- src/components/Provide.tsx
+++
@@ -1,8 +0,0 @@
-// import { useContext } from "react";
-// import { MyContext } from "../context/context";
-
-// export default function Provide() {
-//   const api = useContext(MyContext);
-//   console.log({ api });
-//   return <MyContext.Provider value={api} />;
-// }
