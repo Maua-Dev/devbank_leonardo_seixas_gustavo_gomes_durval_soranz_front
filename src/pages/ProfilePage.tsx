0cb0346a6bb6d7e8616e25afef1cb831398b12f4
GustavoGomes7 <gustavo.ag.2004@gmail.com>
2024-04-04 21:59:29 -0300

    Atualizações das pages

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,3 +1,43 @@
+// import { useContext, useState } from "react";
+// import { VariaveisGlobais } from "../context/context";
+// import AppRouter from "../approuter/AppRouter";
+// import { Link } from "react-router-dom";
+
+
+
+
 export default function ProfilePage() {
-  return <main>Profile Page</main>;
+  // const { api, setApi, conta, setConta,credito, setCredito,agncia,setAgencia } = useContext(VariaveisGlobais);
+
+  const onClickHandler1= () =>{
+    window.location.replace("http://localhost:5173/deposit")
+  }
+  const onClickHandler2= () =>{
+    window.location.replace("http://localhost:5173/withdraw")
+  }
+  const onClickHandler3= () =>{
+    window.location.replace("http://localhost:5173/transfer")
+  }
+  
+  
+  return (
+    <main>
+      <div>
+        <button onClick={onClickHandler1} />
+
+      </div>
+      <div>
+        <button onClick={onClickHandler2} />
+
+      </div>
+      <div>
+        <button onClick={onClickHandler3} />
+
+      </div>
+      
+    </main>
+
+
+  )
 }
+
