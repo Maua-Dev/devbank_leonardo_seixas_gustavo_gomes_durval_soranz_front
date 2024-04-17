f2b7ce4cbe0d93900a57a9141f2ba10eaeef3840
GustavoGomes7 <gustavo.ag.2004@gmail.com>
2024-04-17 19:39:35 -0300

    atualizações de estilização

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+// import Provide from "./Provide";
+
+// export default function Button() {
+//   const onClickHandler = (event) => {
+//     Provide();
+//   };
+
+//   return <button onClick={onClickHandler}> ClickMe </button>;
+// }
